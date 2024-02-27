0367d2b9ec77fbf38e897a47c830c9ba76e9a538
Nirav-Sanepara <nrsanepara@gmail.com>
2024-02-27 06:28:37 +0530

    socket

---
+++ ws.js
@@ -0,0 +1,10 @@
+const x = require('ws')
+const wss =new x.WebSocketServer({port : 8000})
+
+wss.on('connection',(socket)=>{
+    console.log('A new connection');
+    socket.send('Hello from product manager')
+    socket.on('message',(msg)=>{
+        console.log("message : "+msg);
+    })
+})
